e9e51d2c90e05d1c9a94bcfd348478f33ff02edf
Brahima SISSOKO <bsissoko@prosuma.ci>
2025-02-19 17:04:28 +0000

    maj generator

--- UserGenerator/user.py
+++ UserGenerator/user.py
@@ -1,15 +1,37 @@
+"""Module to generate random users"""
 import faker
+import logging
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / 'user.log', level=logging.INFO)
 
 fake = faker.Faker()
 
-def get_user(): #Générer un nom et prenom aléatoire
+def get_user(): 
+    """Generate a single user
+
+    Returns:
+        str : user
+    """
+    logging.info("Generation user.")
     return f"{fake.first_name()} {fake.last_name()}"
 
-def get_users(n):#Générer plusieurs noms et prenoms aléatoires
+def get_users(n):
     # users = []
     # for _ in range(n):
     #    users.append(get_user())
     # return users
+    """Generate a list of users
+
+    Args:
+        n (int):   number of user to generate 
+
+    Returns:
+        list[str]: users
+    """
+    logging.info(f"Generation a list of {n} users.")
     return [get_user() for _ in range(n)]
 
 
