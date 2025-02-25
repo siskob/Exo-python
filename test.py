0bcab7d654d22eb8f3294f0e8a795c4881502998
Brahima SISSOKO <bsissoko@prosuma.ci>
2025-02-25 16:32:29 +0000

    __init__

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-class Voiture:
-    marque = "Lamborghini"
-    
+from dataclasses import dataclass #Pour éviter de répéter les paramètres avec self
+from typing import ClassVar #Pour les attributs de classe
 
-voiture_01 = Voiture()
-voiture_02 = Voiture()
+@dataclass
+class User:
+    first_name : str #Attribut d'instance
+    last_name : str
+    c : ClassVar[int] #Attribut de classe
 
-print(voiture_01.marque)
 
-voiture_01.marque ="Peugeolt"
-voiture_02.marque = "Volkswagen"
-
-print(voiture_01.marque)
-print(voiture_02.marque)
+patrick = User(first_name="Patrick", last_name="Smith")
+print(User.__dict__)
